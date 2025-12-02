2efd3cc0d3d7aa3018c20459d9faacb2e094483c
3AceShowHand <jinl1037@hotmail.com>
2025-12-02 16:16:05 +0800

    remove the verbose log

--- async_producer.go
+++ async_producer.go
@@ -959,7 +959,6 @@ func (bp *brokerProducer) run() {
 			}
 
 			if bp.buffer.wouldOverflow(msg) {
-				DebugLogger.Printf("producer/broker/%d maximum request accumulated, waiting for space\n", bp.broker.ID())
 				if err := bp.waitForSpace(msg, false); err != nil {
 					bp.parent.retryMessage(msg, err)
 					continue
